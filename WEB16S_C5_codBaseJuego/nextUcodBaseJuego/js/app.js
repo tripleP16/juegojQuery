5dbdb0c1820a3e57066d2c917b725a3827121240
Pablo Perez <perez51160900@gmail.com>
2020-02-19 23:04:21 -0400

    25

--- WEB16S_C5_codBaseJuego/nextUcodBaseJuego/js/app.js
+++ WEB16S_C5_codBaseJuego/nextUcodBaseJuego/js/app.js
@@ -15,21 +15,73 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function crearImagen (){
+function crearImagen (id){
     var imagen = document.createElement("img"); 
     imagen.src = imagenes [getRandomInt(0,3)];
+    imagen.id = id;
+    console.log (imagen.id);
     return imagen; 
 }
 
-function rellenarColumna1(){
-    for (var  i =  0; i < 6; i++) {
-        var imagen = crearImagen(); 
-        tablero.col1.push(imagen.src); 
-        $('.col-1').append(imagen); 
+function columnasRelleno(columna, imagen){
+    switch(columna){
+        case 'col-1': tablero.col1.push(imagen);
+                    return '2';
+                        break;
+        case 'col-2': tablero.col2.push(imagen);
+                    return '1,3';
+                        break;
+        case 'col-3': tablero.col3.push(imagen);
+                    return '2,4';
+                        break;  
+        case 'col-4': tablero.col4.push(imagen);
+                    return '3,5';
+                        break;  
+        case 'col-5': tablero.col5.push(imagen);
+                    return '4,6';
+                        break;   
+        case 'col-6': tablero.col6.push(imagen);
+                    return '5,7';
+                        break; 
+        case 'col-7': tablero.col7.push(imagen);
+                    return '6';
+                        break;
     }
+
 }
 
 
+
+
+function rellenarColumna(columna){
+    for (var i= 0; i < 6; i++) {
+        var imagen = crearImagen(i.toString()+columna); 
+        var obtenerAccept = columnasRelleno(columna, imagen);
+        $('.'+columna).append(imagen);
+        $('#'+imagen.id).draggable({
+            containment: '.panel-tablero', 
+            revert : true,
+            revertDuration: 1000
+        });
+        
+        $('#'+imagen.id).droppable({
+            drop: function(){
+
+            }
+        });
+
+    }
+}
+
+function rellenarTablero(){
+   rellenarColumna('col-1');
+   rellenarColumna('col-2');
+   rellenarColumna('col-3');
+   rellenarColumna('col-4');
+   rellenarColumna('col-5');
+   rellenarColumna('col-6');
+   rellenarColumna('col-7');
+}
 $(document).ready(function(){
     var color = $('.main-titulo').css("color");
     setInterval(function(){
@@ -41,6 +93,11 @@ $(document).ready(function(){
         },1200);
         
     }, 500);
-    rellenarColumna1();
+    
+    $('.panel-tablero').hide();
+    rellenarTablero();
+    $('.panel-tablero').slideDown(1500, function(){
+        
+    });
     
 });
