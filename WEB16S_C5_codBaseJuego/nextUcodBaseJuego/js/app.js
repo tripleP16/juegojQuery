129d4cb95af08bcc982ec6c2e5f96c6b00c52378
Pablo Perez <perez51160900@gmail.com>
2020-02-22 01:00:29 -0400

    gg

--- WEB16S_C5_codBaseJuego/nextUcodBaseJuego/js/app.js
+++ WEB16S_C5_codBaseJuego/nextUcodBaseJuego/js/app.js
@@ -1,13 +1,9 @@
 var imagenes = ["image/1.png", "image/2.png", "image/3.png", "image/4.png"];
-var tablero = {
-    "col1": [],
-    "col2": [],
-    "col3": [],
-    "col4": [],
-    "col5": [],
-    "col6": [],
-    "col7": []
+var tablero = new Array(2); 
+for (var i = 0; i<7; i++){
+    tablero[i] = new Array(2);
 }
+
 
 
 function getRandomInt(min, max) {
@@ -19,57 +15,104 @@ function crearImagen (id){
     var imagen = document.createElement("img"); 
     imagen.src = imagenes [getRandomInt(0,3)];
     imagen.id = id;
-   // console.log (imagen.id);
     return imagen; 
 }
 
-function columnasRelleno(columna, imagen){
-    switch(columna){
-        case 'col-1': tablero.col1.push(imagen);
-                    return '2';
-                        break;
-        case 'col-2': tablero.col2.push(imagen);
-                    return '1,3';
-                        break;
-        case 'col-3': tablero.col3.push(imagen);
-                    return '2,4';
-                        break;  
-        case 'col-4': tablero.col4.push(imagen);
-                    return '3,5';
-                        break;  
-        case 'col-5': tablero.col5.push(imagen);
-                    return '4,6';
-                        break;   
-        case 'col-6': tablero.col6.push(imagen);
-                    return '5,7';
-                        break; 
-        case 'col-7': tablero.col7.push(imagen);
-                    return '6';
-                        break;
-    }
-
+function devolverAceptar (i,j){
+    var aceptar  ;
+    var aceptar2;
+    if (j !=6){
+        aceptar = '#' + tablero[i][j+1].id  ;
+        }
+        if (j!=0){
+            aceptar += tablero[i][j-1].id ;
+        }
+        console.log(aceptar);
+        aceptar2 = aceptar;
+    return aceptar2;
 }
 
-
-
-function rellenarColumna(columna){
-    for (var i= 0; i < 6; i++) {
-        var imagen = crearImagen(i.toString()+columna); 
-        var obtenerAccept = columnasRelleno(columna, imagen);
-        $('.'+columna).append(imagen);
-        $('#'+imagen.id).draggable({
-            containment: '.panel-tablero', 
-            revert : true,
-            
-            revertDuration: 1000
-        });
-
+function activarMovimiento(){
     
-        
-       
+    for (var i = 0; i<7; i++){
+        for (var j =0; j<7 ; j++){
+            /*switch (i){
+                case 0 :;
+                            /*if (j!=6){
+                                aceptar = '#' + tablero[i][j+1].id + ' , ';
+                            }
+                            if (j !=0){
+                                aceptar = '#' + tablero[i][j-1].id
+                            }
+                            console.log('#'+tablero[i][j].id);
 
+                            
+                        break;
+                case 1 : break;
+                case 2 : break;
+                case 3 : break;
+                case 4 : break;
+                case 5 : break;
+                case 6 : break;
+            }*/
+            var aceptar = devolverAceptar(i,j);
+            $('#'+tablero[i][j].id).droppable({
+                accept: aceptar,
+                drop: function(){
+                    alert("Hola");
+                }
+            });
+        }
     }
 }
+
+function rellenarTablero(){
+    var imagen ;
+   
+    for (var i = 0; i < 7; i++){
+        for (var j = 0 ; j<7 ; j++){
+            switch(i){
+                case 0: imagen = crearImagen('a'+i.toString() + j.toString());
+                        tablero[i][j] = imagen;
+                        $('.col-1').append(tablero[i][j]);  
+                        break;
+                case 1: imagen = crearImagen(i.toString() + j.toString()+'b');
+                        tablero[i][j] = imagen;
+                        $('.col-2').append(tablero[i][j]);   
+                        break;
+                case 2: imagen = crearImagen(i.toString() + j.toString()+'c');
+                        tablero[i][j] = imagen;
+                        $('.col-3').append(tablero[i][j]);  
+                        break;
+                case 3: imagen = crearImagen(i.toString() + j.toString()+'d');
+                        tablero[i][j] = imagen;
+                        $('.col-4').append(tablero[i][j]); 
+                        break;
+                case 4: imagen = crearImagen(i.toString() + j.toString()+'e');
+                        tablero[i][j] = imagen;
+                        $('.col-5').append(tablero[i][j]);  
+                        break;
+                case 5: imagen = crearImagen(i.toString() + j.toString()+'f');
+                        tablero[i][j] = imagen;
+                        $('.col-6').append(tablero[i][j]);  
+                        break;
+                case 6: imagen = crearImagen(i.toString() + j.toString()+'g');
+                        tablero[i][j] = imagen;
+                        $('.col-7').append(tablero[i][j]);   
+                        break;
+            }
+            
+        }
+    }
+
+    $('.panel-tablero').children().children().draggable({
+        containment : '.panel-tablero',
+        revert : true
+    })
+
+    activarMovimiento();
+}
+
 
 var check =  false ;
 function cronometro(valor, seg, min){
@@ -96,7 +139,8 @@ function cronometro(valor, seg, min){
            },1000)
            $('.score').animate({
             width:'700px'
-        },1000)
+        },1000);
+        clearInterval(intervalo);
        }
        if (seg <10){
         valor = min.toString() + ':'+'0' + seg.toString();
@@ -109,15 +153,7 @@ function cronometro(valor, seg, min){
 
 }
 
-function rellenarTablero(){
-   rellenarColumna('col-1');
-   rellenarColumna('col-2');
-   rellenarColumna('col-3');
-   rellenarColumna('col-4');
-   rellenarColumna('col-5');
-   rellenarColumna('col-6');
-   rellenarColumna('col-7');
-}
+
 $(document).ready(function(){
     var color = $('.main-titulo').css("color");
     setInterval(function(){
@@ -131,18 +167,20 @@ $(document).ready(function(){
     }, 500);
     
     $('.panel-tablero').hide();
-    rellenarTablero();
+   // rellenarTablero();
     $('.panel-tablero').slideDown(1500, function(){
         
     });
     $('.btn-reinicio').click(function(){   
-        cronometro("", 5,0);
+        cronometro("", 0,2);
         
         if ($(this).html() == 'Reiniciar'){
             location.reload(true);
         }
         $(this).html('Reiniciar');
     });
+
+    rellenarTablero();
 
     
         
